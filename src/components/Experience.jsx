f09dce9ef1b082f8aff531f5c2a3ae5f9693d299
Mohammad Jahed Murad Sunny <64752337+juvenileSunny@users.noreply.github.com>
2025-07-23 16:19:21 -0500

    18:00 minutes

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,14 +1,22 @@
 import { OrbitControls } from "@react-three/drei";
 import { Avatar } from "./avatar";
+import { useControls } from "leva";
 
 export const Experience = () => {
+
+ const {animations} = useControls({
+  animations: {
+    value: 'typing',
+    options: ['typing', 'standing', 'falling'],
+  },
+ });
   return (
     <>
       <OrbitControls />
       <group position-y={-1}>
-        <Avatar />
+        <Avatar animations={animations} />
       </group>
-      <ambientLight intensity={2} />
+      <ambientLight intensity={2.5} />
     </>
   );
 };
